b4d4cfe31d57a076b6dd91350160f8c33ed407f3
ChaeMingyun <coalsrbs7@gmail.com>
2019-12-28 21:14:38 +0900

    Add: check functional test can add item

--- test_goat/functional_test.py
+++ test_goat/functional_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,6 +16,20 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), '작업 아이템 입력')
+
+        input_box.send_keys('공작깃털 사기')
+
+        input_box.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in rows)),
+
         self.fail('Finish the test!')
 
 
