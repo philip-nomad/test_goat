baff0afd1d7e5068f11c3af192800ef44d002961
ChaeMingyun <coalsrbs7@gmail.com>
2019-12-30 10:26:36 +0900

    Refactor: functional_test.py using helper method.

--- test_goat/functional_test.py
+++ test_goat/functional_test.py
@@ -7,10 +7,15 @@ from selenium.webdriver.common.keys import Keys
 class NewVisitorTest(unittest.TestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(5)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self) -> None:
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
@@ -24,18 +29,14 @@ class NewVisitorTest(unittest.TestCase):
 
         input_box.send_keys('공작깃털 사기')
         input_box.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('공작깃털을 이용해서 그물 만들기')
         input_box.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
-        self.assertIn(
-            '2: 공작깃털을 이용해서 그물 만들기',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         self.fail('Finish the test!')
 
