cfaae3906f142e4f271d2a2fed0dc598f5a2c1ac
ChaeMingyun <coalsrbs7@gmail.com>
2019-12-30 10:22:52 +0900

    Add: more test codes

--- test_goat/functional_test.py
+++ test_goat/functional_test.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 class NewVisitorTest(unittest.TestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(5)
 
     def tearDown(self) -> None:
         self.browser.quit()
@@ -23,12 +23,19 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(input_box.get_attribute('placeholder'), '작업 아이템 입력')
 
         input_box.send_keys('공작깃털 사기')
+        input_box.send_keys(Keys.ENTER)
 
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('공작깃털을 이용해서 그물 만들기')
         input_box.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in rows)),
+        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+        self.assertIn(
+            '2: 공작깃털을 이용해서 그물 만들기',
+            [row.text for row in rows]
+        )
 
         self.fail('Finish the test!')
 
